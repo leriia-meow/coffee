f6ffe4e4835aabc0571b6d5eba39b310d6da1f59
aleshinave.23 <leriia-spb@yandex.ru>
2022-02-01 12:55:29 +0300

    Почти сделано

--- main.py
+++ main.py
@@ -9,14 +9,77 @@ from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 import sqlite3
 
 
+class Ui_MainWindow5(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(315, 278)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(230, 210, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 47, 13))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 81, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 120, 47, 13))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(20, 170, 47, 13))
+        self.label_4.setObjectName("label_4")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(100, 20, 171, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(100, 70, 171, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(100, 170, 171, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(100, 120, 171, 22))
+        self.comboBox.setObjectName("comboBox")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 210, 91, 16))
+        self.label_5.setText("")
+        self.label_5.setObjectName("label_5")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 315, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить"))
+        self.label.setText(_translate("MainWindow", "Название"))
+        self.label_2.setText(_translate("MainWindow", "Год выпуска"))
+        self.label_3.setText(_translate("MainWindow", "Жанр"))
+        self.label_4.setText(_translate("MainWindow", "Длина"))
+
+
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("coffee")
         MainWindow.resize(427, 388)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 0, 75, 23))
+        self.pushButton.setObjectName("pushButton")
         self.tableView = QtWidgets.QTableView(self.centralwidget)
-        self.tableView.setGeometry(QtCore.QRect(10, 10, 411, 331))
+        self.tableView.setGeometry(QtCore.QRect(10, 25, 411, 331))
         self.tableView.setObjectName("tableView")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -33,12 +96,17 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("coffee", "coffee"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить"))
 
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.new)
+        self.rez()
+
+    def rez(self):
         db = QSqlDatabase.addDatabase('QSQLITE')
         db.setDatabaseName('coffee.db')
         db.open()
@@ -48,10 +116,50 @@ class Window(QMainWindow, Ui_MainWindow):
         model.select()
         view.setModel(model)
 
+    def new(self):
+        self.win = Winn()
+        self.win.show()
+        self.win.pushButton.clicked.connect(self.rez)
+
+
+class Winn(QMainWindow, Ui_MainWindow5):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.con = sqlite3.connect('coffee.db')
+        self.cur = self.con.cursor()
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label_5.setText("")
+        a = self.lineEdit.text()
+        b = self.lineEdit_2.text()
+        c = self.lineEdit_3.text()
+        d = self.comboBox.currentText()
+        g = self.cur.execute('''SELECT * FROM coffee''').fetchall()
+        d1 = {}
+        for i in g:
+            d1[i[1]] = i[0]
+            print(i)
+        if a and b and c:
+            try:
+                b = int(b)
+                c = int(c)
+                if -1 < b < 2022 and -1 < c:
+                    self.cur.execute("""INSERT INTO coffee(name, year, genre, duration) VALUES(?, ?, ?, ?)""",
+                                     (a, b, d1[d], c))
+                    self.con.commit()
+                    self.con.close()
+                    self.close()
+                else:
+                    self.label_5.setText("Неверный формат ввода")
+            except Exception:
+                self.label_5.setText("Неверный формат ввода")
+        else:
+            self.label_5.setText("Неверный формат ввода")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     wndw = Window()
     wndw.show()
     app.exec()
-
